b617b99df4b68a40c457722d418cfd178d377e00
OceaneLao <oceane.lao1@gmail.com>
2023-12-07 16:32:44 +0100

    merge

--- src/components/Page/CharactersSection.vue
+++ src/components/Page/CharactersSection.vue
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-
-import { toLower } from 'cypress/types/lodash';
-=======
->>>>>>> 78afdc9d24546cc2c6f16e8083dfdd3dfdd1e1ef
 <script>
     import {useCharactersStore} from '@/stores'
     import CreationView from '../Content/CreationView.vue';
@@ -21,12 +16,7 @@ import { toLower } from 'cypress/types/lodash';
 </script>
 
 <template>
-<<<<<<< HEAD
-    <h1>Personnages</h1>
-    <creation-view />
-=======
     <gallery-view v-if="!creationViewActivated"/>
     <creation-view v-else />
     <button @click="creationViewActivated = !creationViewActivated">Changer de vue</button>
->>>>>>> 78afdc9d24546cc2c6f16e8083dfdd3dfdd1e1ef
 </template>

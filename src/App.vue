7ea990bad2e0dbb63972f9ba198d5391f0779fc7
Roman Tolstokoulakov <roman.tolstokoulakov@gmail.com>
2023-12-11 14:26:44 +0100

    Added some css changes to Home Pages

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
-  import { RouterView } from 'vue-router'
-  import MainLayout from '@/components/Layout/MainLayout.vue'
-  import MainNav from '@/components/Layout/MainNav.vue'
-  import FooterView from '@/components/Layout/FooterView.vue'
+import { RouterView } from 'vue-router'
+import MainLayout from '@/components/Layout/MainLayout.vue'
+import MainNav from '@/components/Layout/MainNav.vue'
+import FooterView from '@/components/Layout/FooterView.vue'
 </script>
 
 <template>
@@ -20,28 +20,5 @@
 </template>
 
 <style>
-/* Ajoutez la classe Bootstrap au body */
-@import url('https://fonts.googleapis.com/css2?family=MedievalSharp&family=Whisper&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Hedvig+Letters+Sans&display=swap');
 
-.navPad {
-  margin-left: 45px;
-  text-decoration: none;
-  color: #ffffff;
-  font-size: 24px;
-  font-family: 'MedievalSharp', cursive;
-}
-
-a:hover {
-  color: #fd923b;
-}
-
-body {
-  background-color: rgb(59, 56, 62) !important;
-  color: #ffff;
-  
-  p{
-    font-family: 'Hedvig Letters Sans', sans-serif;
-  }
-}
 </style>

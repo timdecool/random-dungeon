8f43c5e3a54d62b89e65a2672ed1cdf530d69bdd
Roman Tolstokoulakov <roman.tolstokoulakov@gmail.com>
2023-12-15 16:37:33 +0100

    branche cassé

---
+++ src/components/Content/CharacterInfos.vue
@@ -0,0 +1,45 @@
+<script>
+import { useCharactersStore } from '@/stores';
+import { mapState } from 'pinia';
+const character = computed(useCharactersStore, ['characters']);
+
+</script>
+
+<template>
+  <h2>Détails de {{ characters.name }}</h2>
+
+  <ul class               = "mb-2">
+    <li>
+      Ascendance          : {{ characters.ascendancy }}
+    </li>
+    <li>
+      Nom                 : {{ characters.name }}
+    </li>
+    <li>
+      Voie                : {{ characters.charClass }}
+    </li>
+    <li>
+      Capacité spéciale   : {{ characters.feat }}
+    </li>
+    <li>
+      Arme                : {{ characters.weapon }}
+    </li>
+    <li>
+      Historique          : {{ characters.background }}
+    </li>
+    <li>
+      Défaut              : {{ characters.flaw }}
+    </li>
+    <li>
+      Babiole             : {{ characters.trinket }}
+    </li>
+  </ul>
+</template>
+
+<style>
+ul {
+  list-style-type : none;
+  margin : 0;
+  padding : 0;
+}
+</style>
--- src/views/CharacterDetails.vue
+++ src/views/CharacterDetails.vue
@@ -1,7 +1,33 @@
 <script setup>
+import { useCharactersStore } from '@/stores/';
+import CharacterInfos  from '../components/Content/CharacterInfos.vue' // componant de charachter info permettant de voir le personnage 
+
+// initialise
+const characterStore = useCharactersStore();
+
+characterStore.characters;
 
 </script>
 
 <template>
-    <h1>Coucou</h1>    
+
+<section class="d-flex flex-column align-items-start col-4 p-2 CharacterInfos">
+      <character-infos/>      
+      <router-link to="/characters/gallery" class="btn btn-secondary">Retour à la galerie</router-link>
+    </section>
+<pre>
+ {{characterStore.characters}}
+</pre>
+
+
+
 </template>
+
+<style scoped>
+.CharacterInfos {
+  background-color: #3b3742;
+  border: solid 1px rgb(253, 253, 212);
+  border-radius: 5px;
+
+}
+</style>
